ac768a4dad78877b8daed6521965903c1577fe54
AryasatyaWidipratama <aryasatya2721@gmail.com>
2021-09-27 22:29:29 +0700

    committesting

--- resources/views/kategori.blade.php
+++ resources/views/kategori.blade.php
@@ -10,7 +10,7 @@
 
         <table border="1">
             <tr>
-          
+          testtesttest
                 <th>ID kategori bencana</th>
                 <th>KATEGORI BENCANA</th>
             </tr>
