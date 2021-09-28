e66917abe33ce0fe641ef98ddbe13ce3c0fead70
AryasatyaWidipratama <aryasatya2721@gmail.com>
2021-09-28 08:54:48 +0700

    Commit4

--- resources/views/pwbf.blade.php
+++ resources/views/pwbf.blade.php
@@ -1,36 +1,36 @@
 <!DOCTYPE html>
-<html lang="en">
+<html >
 <body>
     <h3>PWBF KELOMPOK 10</h3>
 
-                <a class="nav-link active" aria-current="page" href="/kecamatan">Data Kecamatan</a>
+                <a href="/kecamatan">Data Kecamatan</a>
                 <br/>
                 <br/>
-                <a class="nav-link active" aria-current="page" href="/bencana">Data Bencana</a>
+                <a href="/bencana">Data Bencana</a>
                 <br/>
                 <br/>
-                <a class="nav-link active" aria-current="page" href="/kota">Data Kota</a>
+                <a  href="/kota">Data Kota</a>
                 <br/>
                 <br/>
-                <a class="nav-link active" aria-current="page" href="/kategori">Data Kategori Bencana</a>
+                <a  href="/kategori">Data Kategori Bencana</a>
                 <br/>
                 <br/>
-                <a class="nav-link active" aria-current="page" href="/detail">Detail Korban</a>
+                <a  href="/detail">Detail Korban</a>
                 <br/>
                 <br/>
-                <a class="nav-link active" aria-current="page" href="/pelaporan">pelaporan</a>
+                <a  href="/pelaporan">pelaporan</a>
                 <br/>
                 <br/>
-                <a class="nav-link active" aria-current="page" href="/provinsi">Data provinsi</a>
+                <a href="/provinsi">Data provinsi</a>
                 <br/>
                 <br/>
-                <a class="nav-link active" aria-current="page" href="/users">Data User</a>
+                <a  href="/users">Data User</a>
                 <br/>
                 <br/>
-                <a class="nav-link active" aria-current="page" href="/role">Data role</a>
+                <a  href="/role">Data role</a>
                 <br/>
                 <br/>
-                <a class="nav-link active" aria-current="page" href="/user_role">Data user role</a>
+                <a href="/user_role">Data user role</a>
                 <br/>
                 <br/>
     
